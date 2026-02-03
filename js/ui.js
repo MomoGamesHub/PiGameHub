9a0b3a381bfad8ab8b2a9e95fcbe78a46cff311e
MomoGamesHub <samir.momo82@gmail.com>
2026-02-03 22:34:43 +0200

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,37 @@
+const UI = {
+  showModal(id) {
+    document.getElementById(id).style.display = "flex";
+  },
+
+  closeModals() {
+    document.querySelectorAll(".modal").forEach(m => m.style.display="none");
+  },
+
+  updateUserUI(user) {
+    document.getElementById("usernameDisplay").innerText =
+      "مرحباً: " + user.username;
+
+    document.getElementById("loginBtn").style.display = "none";
+    document.getElementById("logoutBtn").style.display = "inline-block";
+  },
+
+  showProfile() {
+    let u = Storage.getUser();
+    if (!u) return;
+
+    document.getElementById("p_username").innerText = u.username;
+    document.getElementById("p_uid").innerText = u.uid;
+    document.getElementById("p_points").innerText = u.points;
+
+    let h = document.getElementById("gameHistory");
+    h.innerHTML = "";
+
+    u.history.forEach(i => {
+      let li = document.createElement("li");
+      li.innerText = i;
+      h.appendChild(li);
+    });
+
+    this.showModal("profileModal");
+  }
+};
